1d11755865167517cf99a4aeb866527d6700b87a
Gabriel Larramendi <larramendi.dev@gmail.com>
2022-09-22 12:27:06 -0300

    Exercicio - leitura de arquivos finalizado - pt1

--- exercise-csv/src/application/Program.java
+++ exercise-csv/src/application/Program.java
@@ -1,31 +1,59 @@
 package application;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import entities.Product;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("Digite o nome do arquivo + .extensão: ");
-        String path = "/home/larramendi/Documents/java-course/trabalhando-com-arquivos/" + sc.nextLine();
+        List<Product> products = new ArrayList<>();
 
-        System.out.println(path);
-        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+        System.out.print("Enter file path: ");
+        String sourceFileStr = sc.nextLine();
 
-            String line = br.readLine();
+        File sourceFile = new File(sourceFileStr);
 
-            while (line != null) {
-                System.out.println(line);
-                line = br.readLine();
+        String sourceFolderStr = sourceFile.getParent();
+
+        boolean success = new File(sourceFolderStr + "/out").mkdir();
+        System.out.println("Folder created: " + success);
+
+        String targetFileStr = sourceFolderStr + "/out/summary.csv";
+        sc.close();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(sourceFileStr))) {
+
+            String itemCsv = br.readLine();
+
+            while (itemCsv != null) {
+
+                String[] fields = itemCsv.split(",");
+                String name = fields[0];
+                double price = Double.parseDouble(fields[1]);
+                int quantity = Integer.parseInt(fields[1 + 1]);
+
+                products.add(new Product(name, price, quantity));
+                itemCsv = br.readLine();
+            }
+
+            try (BufferedWriter bw = new BufferedWriter(new FileWriter(targetFileStr))){
+                for (Product p : products) {
+                    bw.write(p.getName() + "," + p.total());
+                    bw.newLine();
+                }
+            }
+            catch (IOException e) {
+                System.out.println("Error writing file: " + e.getMessage());
             }
         }
         catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("Error writing file: " + e.getMessage());
         }
-
 
     }
 }
